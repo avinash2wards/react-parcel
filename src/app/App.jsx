bbfdb4bbd392a6d511d3867a3933a03760dba233
Avinash C <avinash2wards@outlook.com>
2018-06-16 18:40:12 +0530

    Elephant addedt

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { HelloWorld } from "./HelloWorld";
+import elephant from "./img/elephant.gif";
 
 class App extends Component {
   state = {
@@ -8,9 +9,14 @@ class App extends Component {
   };
 
   render() {
+    let center = {
+      display: "table",
+      margin: "0 auto"
+    };
     return (
       <div>
         <HelloWorld greet={this.state.greet} name={"Avinash"} />;
+        <img style={center} src={elephant} alt="elephant" />
       </div>
     );
   }
